ae2c0439f7efab8f96bc14c147f45ef1b7a6f73b
Tarek <bayridge4th@gmail.com>
2019-03-20 20:48:44 -0400

    added environment var port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/ping',(req,res)=>{
     res.json({'pong':'🏓'})
 })
 
-const port = 6001;
+const port = process.env.PORT || 6001;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
 })
